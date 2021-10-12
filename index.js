4c52e1e2a99cdc4e0aba9ede130bc7917f7f0dc0
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-10-12 15:32:46 +0700

    todo-list-service for aws practice

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/todo-list");
+
+const Todo = mongoose.model(
+  "Todo",
+  new Schema({
+    title: String,
+    isFinish: Boolean,
+  })
+);
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello.");
+});
+
+app.get("/todo-list", async (req, res) => {
+  const todos = await Todo.find();
+  res.send(
+    todos.map((todo) => {
+      return {
+        id: todo._id.toString(),
+        title: todo.title,
+        isFinish: todo.isFinish,
+      };
+    })
+  );
+});
+
+app.post("/todo-list", async (req, res) => {
+  const { title } = req.body;
+  const newTodo = new Todo({ title, isFinish: false });
+  await newTodo.save();
+  res.sendStatus(200);
+});
+
+app.put("/todo-list/:id", async (req, res) => {
+  const { id } = req.params;
+  const todo = await Todo.findById(id);
+  await todo.update({ isFinish: !todo.isFinish });
+  res.sendStatus(200);
+});
+
+app.delete("/todo-list/:id", async (req, res) => {
+  const { id } = req.params;
+  await Todo.findByIdAndDelete(id);
+  res.sendStatus(200);
+});
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
